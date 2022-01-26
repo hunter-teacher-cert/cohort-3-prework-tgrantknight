63f1fdac4058b9191c651cfcfd0df1b129b9bc74
Taylor Grant-Knight <97416649+tgrantknight@users.noreply.github.com>
2022-01-26 21:56:12 +0000

    pre05 finished

---
+++ pre05/Methods.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Methods {
+  // Main method to test custom methods
+  public static void main(String[] args){
+    int x, y, z;
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter value for m:");
+    x = in.nextInt();
+    System.out.println("Enter value for n:");
+    y = in.nextInt();
+
+    System.out.println(ack(x,y));
+  }
+
+  public static boolean isDivisible(int n, int m){
+    if ((n % m) == 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static boolean isTriangle(int a, int b, int c){
+    if ((c > a + b) || (b > a + c) || (a > b + c)){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public static int ack(int m, int n){
+    if (m == 0){
+      return n+1;
+    } else if (m > 0 && n == 0){
+      return ack(m-1,1);
+    } else if (m > 0 && n > 0){
+      return ack(m-1,ack(m,n-1));
+    } else {
+      System.out.println("Please enter two non-negative integers");
+      // Not sure of a better way to catch the error at this point
+      return -1;
+    }
+  }
+}
