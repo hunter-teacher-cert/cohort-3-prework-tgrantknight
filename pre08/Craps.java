b663867019a6700c1daab53303a024a134a2cfae
Taylor Grant-Knight <97416649+tgrantknight@users.noreply.github.com>
2022-01-27 21:59:47 +0000

    finished craps, TODO: clean up and document

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -7,14 +7,17 @@ public class Craps {
     Scanner in = new Scanner(System.in);
     int n,m, result;
 
-    System.out.println("Enter a dice size:");
-    n = in.nextInt();
-    System.out.println("Enter a number of dice to roll:");
-    m = in.nextInt();
+    //System.out.println("Enter a dice size:");
+    //n = in.nextInt();
+    //System.out.println("Enter a number of dice to roll:");
+    //m = in.nextInt();
 
-    result = shoot(m,n);
-    System.out.printf("Your max roll is: %d", result);
-    System.out.println();
+    boolean win = round();
+    if(win){
+      System.out.println("You win!");
+    } else {
+      System.out.println("You lose!");
+    }
   }
 
   public static int roll(int diceSize){
@@ -24,17 +27,40 @@ public class Craps {
   }
 
   public static int shoot(int amt, int diceSize){
-    int max = 0;
+    int sum = 0;
     for (int i = 0; i < amt; i++){
-      int currentRoll = roll(diceSize);
-      if (max < currentRoll){
-        max = currentRoll;
-      }
+      sum = sum + roll(diceSize);
     }
-    return max;
+    return sum;
   }
   //TODO
-  public static boolean round(){
-    return true;
+  public static boolean round(){ 
+    int diceRoll = shoot(2,6);
+    System.out.printf("First: Rolled a %d", diceRoll);
+    System.out.println();
+    int point = 0;
+    if (diceRoll == 2 || diceRoll == 3 || diceRoll == 12){
+      return false;
+    } else if (diceRoll == 7 || diceRoll == 11) {
+      return true;
+    } else {
+      point = diceRoll;
+    }
+    System.out.printf("Point is: %d",point);
+    System.out.println();
+    do {
+      diceRoll = shoot(2,6);
+      System.out.printf("First: Rolled a %d", diceRoll);
+      System.out.println();
+    } while (diceRoll != 7 && diceRoll != point);
+
+    if (diceRoll == 7){
+      return false;
+    } else if (diceRoll == point) {
+      return true;
+    } else {
+      System.out.println("Uh oh... returning false but error");
+      return false;
+    }
   }
 }
